d2710863e20f502c6d294cf2464cce0f9ee0fc75
Simon <simon_petersen1994@hotmail.com>
2016-06-22 17:50:11 +0200

    Der tilføjes SelectArcs ved XOR-split/join og OR-split.

--- dtu.mbse.groupi.yawl.simulator/src/dtu/mbse/groupi/yawl/simulator/application/EnabledTransitionHandler.java
+++ dtu.mbse.groupi.yawl.simulator/src/dtu/mbse/groupi/yawl/simulator/application/EnabledTransitionHandler.java
@@ -1,21 +1,37 @@
 package dtu.mbse.groupi.yawl.simulator.application;
 
+import java.util.Map;
+
 import org.eclipse.draw2d.MouseEvent;
+import org.pnml.tools.epnk.annotations.netannotations.NetAnnotation;
 import org.pnml.tools.epnk.annotations.netannotations.ObjectAnnotation;
 import org.pnml.tools.epnk.applications.ui.IActionHandler;
+
+import dtu.mbse.groupi.yawl.Place;
+import dtu.mbse.groupi.yawl.Transition;
+import dtu.mbse.groupi.yawlsimulator.EnabledTransition;
 
 public class EnabledTransitionHandler implements IActionHandler {
 
 	private YawlSimulatorApplication application;
-	
+
 	public EnabledTransitionHandler(YawlSimulatorApplication application) {
 		super();
 		this.application = application;
 	}
-	
+
 	@Override
 	public boolean mouseDoubleClicked(MouseEvent arg0, ObjectAnnotation annotation) {
-		// TODO Auto-generated method stub
+		if (annotation instanceof EnabledTransition) {
+			Object object = annotation.getObject();
+			if (object instanceof Transition) {
+				Transition transition = (Transition) object;
+				Map<Place, Integer> marking = application.computeMarking();
+				marking = application.fireTransition(marking, transition);
+				NetAnnotation netAnnotation = application.computeAnnotation(marking);
+				application.getNetAnnotations().setCurrent(netAnnotation);
+			}
+		}
 		return false;
 	}
 
--- dtu.mbse.groupi.yawl.simulator/src/dtu/mbse/groupi/yawl/simulator/application/YawlSimulatorApplication.java
+++ dtu.mbse.groupi.yawl.simulator/src/dtu/mbse/groupi/yawl/simulator/application/YawlSimulatorApplication.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.pnml.tools.epnk.annotations.netannotations.NetAnnotation;
 import org.pnml.tools.epnk.annotations.netannotations.NetannotationsFactory;
+import org.pnml.tools.epnk.annotations.netannotations.ObjectAnnotation;
 import org.pnml.tools.epnk.applications.ApplicationWithUIManager;
 import org.pnml.tools.epnk.applications.ui.ApplicationUIManager;
 import org.pnml.tools.epnk.helpers.FlatAccess;
@@ -12,17 +13,20 @@ import org.pnml.tools.epnk.pnmlcoremodel.Arc;
 import org.pnml.tools.epnk.pnmlcoremodel.Node;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
 import org.pnml.tools.epnk.pnmlcoremodel.PlaceNode;
-import org.pnml.tools.epnk.pnmlcoremodel.Transition;
 
 import dtu.mbse.groupi.yawl.Place;
+import dtu.mbse.groupi.yawl.Transition;
 import dtu.mbse.groupi.yawl.TransitionTypes;
 import dtu.mbse.groupi.yawl.util.YawlFunctions;
 import dtu.mbse.groupi.yawlsimulator.EnabledTransition;
 import dtu.mbse.groupi.yawlsimulator.Marking;
+import dtu.mbse.groupi.yawlsimulator.SelectArc;
 import dtu.mbse.groupi.yawlsimulator.YawlsimulatorFactory;
 
 public class YawlSimulatorApplication extends ApplicationWithUIManager {
 
+	private FlatAccess flatNet;
+	
 	public YawlSimulatorApplication(PetriNet petrinet) {
 		super(petrinet);
 		getNetAnnotations().setName("Yawl Simulator");
@@ -30,13 +34,20 @@ public class YawlSimulatorApplication extends ApplicationWithUIManager {
 		manager.addActionHandler(new SelectArcHandler(this));
 		manager.addActionHandler(new EnabledTransitionHandler(this));
 		manager.addPresentationHandler(new YawlSimulatorPresentationHandler());
+		setFlatNet();
+	}
+	
+	public void setFlatNet() {
+		if (flatNet == null) {
+			flatNet = new FlatAccess(this.getPetrinet());
+		}
 	}
 
 	@Override
 	protected void initializeContents() {
 		FlatAccess flatNet = new FlatAccess(this.getPetrinet());
 		Map<Place, Integer> initialMarking = computeInitialMarking(flatNet);
-		NetAnnotation initialAnnotation = computeAnnotation(flatNet, initialMarking);
+		NetAnnotation initialAnnotation = computeAnnotation(initialMarking);
 		initialAnnotation.setNet(this.getPetrinet());
 		this.getNetAnnotations().getNetAnnotations().add(initialAnnotation);
 		this.getNetAnnotations().setCurrent(initialAnnotation);
@@ -55,8 +66,24 @@ public class YawlSimulatorApplication extends ApplicationWithUIManager {
 		}
 		return marking;
 	}
+	
+	Map<Place, Integer> computeMarking() {
+		Map<Place, Integer> marking = new HashMap<Place, Integer>();
+		NetAnnotation annotation = this.getNetAnnotations().getCurrent();
+		for (ObjectAnnotation objectAnnotation : annotation.getObjectAnnotations()) {
+			if (objectAnnotation instanceof Marking) {
+				Marking markingAnnotation = (Marking) objectAnnotation;
+				Object object = markingAnnotation.getObject();
+				if (object instanceof Place) {
+					marking.put((Place) object, markingAnnotation.getValue()); 
+				}
+			}
+		}
+		return marking;
+	}
 
-	NetAnnotation computeAnnotation(FlatAccess flatNet, Map<Place, Integer> marking) {
+	NetAnnotation computeAnnotation(Map<Place, Integer> marking) {
+		setFlatNet();
 		NetAnnotation annotation = NetannotationsFactory.eINSTANCE.createNetAnnotation();
 		for (Place place : marking.keySet()) {
 			if (place instanceof Place) {
@@ -70,19 +97,35 @@ public class YawlSimulatorApplication extends ApplicationWithUIManager {
 			}
 		}
 
-		for (Node transition : flatNet.getTransitions()) {
-			if (transition instanceof Transition) {
-				if (enabled(flatNet, marking, (Transition) transition)) {
+		for (Node node : flatNet.getTransitions()) {
+			if (node instanceof Transition) {
+				Transition transition = (Transition) node;
+				if (enabled(marking, transition)) {
 					EnabledTransition transitionAnnotation = YawlsimulatorFactory.eINSTANCE.createEnabledTransition();
 					transitionAnnotation.setObject(transition);
 					annotation.getObjectAnnotations().add(transitionAnnotation);
+					if (transition.getSplitType().getText() == TransitionTypes.OR ||
+							transition.getSplitType().getText() == TransitionTypes.XOR) {
+						for (Arc arc : transition.getOut()) {
+							SelectArc selectArc = YawlsimulatorFactory.eINSTANCE.createSelectArc();
+							selectArc.setObject(arc);
+							annotation.getObjectAnnotations().add(selectArc);
+						}
+					}
+					if (transition.getJoinType().getText() == TransitionTypes.XOR) {
+						for (Arc arc : transition.getIn()) {
+							SelectArc selectArc = YawlsimulatorFactory.eINSTANCE.createSelectArc();
+							selectArc.setObject(arc);
+							annotation.getObjectAnnotations().add(selectArc);
+						}
+					}
 				}
 			}
 		}
 		return annotation;
 	}
 
-	boolean enabled(FlatAccess flatNet, Map<Place, Integer> marking, Transition transition) {
+	boolean enabled(Map<Place, Integer> marking, Transition transition) {
 		if (transition instanceof dtu.mbse.groupi.yawl.Transition) {
 			dtu.mbse.groupi.yawl.Transition yawlTransition = (dtu.mbse.groupi.yawl.Transition) transition;
 			if (yawlTransition.getJoinType().getText() == TransitionTypes.AND) {
@@ -101,8 +144,9 @@ public class YawlSimulatorApplication extends ApplicationWithUIManager {
 						}
 					}
 				}
-			return true;
-			} else if (yawlTransition.getJoinType().getText() == TransitionTypes.OR) {
+				return true;
+			} 
+			else {
 				for (Arc arc : flatNet.getIn(transition)) {
 					if (arc instanceof Arc) {
 						Object source = arc.getSource();
@@ -118,10 +162,60 @@ public class YawlSimulatorApplication extends ApplicationWithUIManager {
 						}
 					}
 				}
-			return false;
+				return false;
 			}
 		}
 		return false;
+	}
+
+	Map<Place, Integer> fireTransition(Map<Place, Integer> marking1, Transition transition) {
+		setFlatNet();
+		Map<Place, Integer> marking2 = new HashMap<Place, Integer>();
+		for (Place place: marking1.keySet()) {
+			marking2.put(place, marking1.put(place, marking1.get(place)));
+		}
+		
+		// If AND-join:
+		for (Arc arc: flatNet.getIn(transition)) {
+			if (arc instanceof dtu.mbse.groupi.yawl.Arc) {
+				Arc yawlArc = (dtu.mbse.groupi.yawl.Arc) arc;
+				Object source  = yawlArc.getSource();
+				if (source instanceof PlaceNode) {
+					source = flatNet.resolve((PlaceNode) source);
+					if (source instanceof dtu.mbse.groupi.yawl.Place) {
+						Place place = (Place) source;
+						int available = 0;
+						if (marking1.containsKey(place)) {
+							available = marking1.get(place);
+						}
+						int needed = 1;
+						marking2.put(place, available-needed);
+					}
+				}
+			}
+		}
+		
+		// If AND-split
+		for (Arc arc: flatNet.getOut(transition)) {
+			if (arc instanceof dtu.mbse.groupi.yawl.Arc) {
+				Arc yawlArc = (dtu.mbse.groupi.yawl.Arc) arc;
+				Object target  = yawlArc.getTarget();
+				if (target instanceof PlaceNode) {
+					target = flatNet.resolve((PlaceNode) target);
+					if (target instanceof Place) {
+						Place place = (Place) target;
+						int available = 0;
+						if (marking1.containsKey(place)) {
+							available = marking1.get(place);
+						}
+						int provided = 1; 
+						marking2.put(place, available+provided);
+					}
+				}
+			}
+		}
+
+		return marking2;
 	}
 
 }
